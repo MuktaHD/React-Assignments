38eb2932ea0f36efd30ca18f38110e08f0103f98
MuktaHD <muktabhosale3@gmail.com>
2024-08-05 16:23:10 +0530

    react assign 16-17

---
+++ assignment16-17/src/translations.js
@@ -0,0 +1,25 @@
+// src/translations.js
+export const translations = {
+    en: {
+      welcome: "Welcome",
+      login: "Login",
+      logout: "Logout",
+      
+      switchToMarathi: "Switch to Marathi",
+      addNotification: "Add Notification",
+      currentLanguage: "Current language",
+      settingsExample: "Settings example"
+    },
+   
+    marathi: {
+      welcome: "स्वागत आहे",
+      login: "लॉगिन करा",
+      logout: "लॉगआउट करा",
+     
+      switchToMarathi: "मराठीत बदला",
+      addNotification: "सूचना जोडा",
+      currentLanguage: "सध्याची भाषा",
+      settingsExample: "सेटिंग्जचे उदाहरण"
+    }
+  };
+  
