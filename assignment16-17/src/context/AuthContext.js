9c8dd484bb4190aee708bc2a9e07bd677661feab
MuktaHD <165270785+MuktaHD@users.noreply.github.com>
2024-08-12 22:52:36 +0530

    Update AuthContext.js

--- assignment16-17/src/context/AuthContext.js
+++ assignment16-17/src/context/AuthContext.js
@@ -7,7 +7,6 @@ export const AuthProvider = ({ children }) => {
 
   const login = (userData) => setUser(userData);
   const logout = () => setUser(null);
-
   return (
     <AuthContext.Provider value={{ user, login, logout }}>
       {children}
